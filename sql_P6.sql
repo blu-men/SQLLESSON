f6ef592141a4798e83ab24275751e388013f09a0
blu-men <cjbb5eyv@icloud.com>
2021-08-17 16:37:50 +0900

    initial_commit

--- sql_P6.sql
+++ sql_P6.sql
@@ -1,2 +1,7 @@
-SELECT CASE WHEN 
-  FROM poptbl END;
+SELECT CASE WHEN population < 100 THEN '01'
+            WHEN population >= 100 AND population < 200 THEN '02'
+            WHEN population >= 200 AND population < 300 THEN '03'
+            WHEN population >= 300 THEN '04'
+        ELSE NULL END AS pop_class,
+        COUNT(*) AS cnt
+  FROM poptbl;
