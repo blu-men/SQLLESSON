63acc0f27ab73bbc55090ff10156dabc6104cb7f
blu-men <cjbb5eyv@icloud.com>
2021-08-19 11:04:23 +0900

    add_DB_files

--- sql.sql
+++ sql.sql
@@ -1,12 +1,10 @@
-SELECT CASE pref_name
-    WHEN '徳島' THEN '四国'
-    WHEN '香川' THEN '四国'
-    WHEN '愛媛' THEN '四国'
-    WHEN '高知' THEN '四国'
-    WHEN '福岡' THEN '九州'
-    WHEN '佐賀' THEN '九州'
-    WHEN '長崎' THEN '九州'
-  ELSE 'その他' END AS district,
-  SUM(population)
-  FROM poptbl
-GROUP BY district;
+BEGIN TRANSACTION;
+INSERT INTO Shohin VALUES ('0001', 'Tシャツ', '衣服', 1000, 500, '2009-09-20');
+INSERT INTO Shohin VALUES ('0002', '穴あけパンチ', '事務用品', 500, 320, '2009-09-11');
+INSERT INTO Shohin VALUES ('0003', 'カッターシャツ', '衣服', 4000, 2800, NULL);
+INSERT INTO Shohin VALUES ('0004', '包丁', 'キッチン用品', 3000, 2800, '2009-09-20');
+INSERT INTO Shohin VALUES ('0005', '圧力鍋', 'キッチン用品', 6800, 5000, '2009-01-15');
+INSERT INTO Shohin VALUES ('0006', 'フォーク', 'キッチン用品', 500, NULL, '2009-09-20');
+INSERT INTO Shohin VALUES ('0007', 'おろしがね', 'キッチン用品', 880, 790, '2008-04-28');
+INSERT INTO Shohin VALUES ('0008', 'ボールペン', '事務用品', 100, NULL, '2009-11-11');
+COMMIT;
