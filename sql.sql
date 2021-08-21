e56df88c9947c7168d75d19ea0cd862b78a54058
blu-men <cjbb5eyv@icloud.com>
2021-08-21 18:02:34 +0900

    add_DB_files

--- sql.sql
+++ sql.sql
@@ -1,2 +1,1 @@
-INSERT INTO shohinins (shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka, shiire_tanka, torokubi) VALUES
-('0001', 'Tシャツ', '衣服', 1000, 500, '2009-09-20');
+DELETE FROM shohin WHERE hanbai_tanka >= 4000;
