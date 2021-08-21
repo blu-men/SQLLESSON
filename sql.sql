861645433f3f08907e8f71c60cc562908202d7b8
blu-men <cjbb5eyv@icloud.com>
2021-08-21 16:20:35 +0900

    add_DB_files

--- sql.sql
+++ sql.sql
@@ -1,2 +1,8 @@
-SELECT shohin_bunrui, SUM(hanbai_tanka), SUM(shiire_tanka) FROM shohin GROUP BY shohin_bunrui 
-HAVING SUM(hanbai_tanka) > SUM(shiire_tanka) * 1.5;
+CREATE TABLE shohinins
+  (shohin_id    CHAR(4)      NOT NULL,
+  shohin_mei    VARCHAR(100) NOT NULL,
+  shohin_bunrui VARCHAR(32)  NOT NULL,
+  hanbai_tanka  INTEGER      DEFAULT 0,
+  shiire_tanka  INTEGER      ,
+  torokubi      DATE         ,
+  PRIMARY KEY (shohin_id));
