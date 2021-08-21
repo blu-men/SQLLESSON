5893f2b842fb0153f22904998d189c29a96c1215
blu-men <cjbb5eyv@icloud.com>
2021-08-21 17:08:15 +0900

    add_DB_files

--- sql.sql
+++ sql.sql
@@ -1,8 +1,2 @@
-CREATE TABLE shohinins
-  (shohin_id    CHAR(4)      NOT NULL,
-  shohin_mei    VARCHAR(100) NOT NULL,
-  shohin_bunrui VARCHAR(32)  NOT NULL,
-  hanbai_tanka  INTEGER      DEFAULT 0,
-  shiire_tanka  INTEGER      ,
-  torokubi      DATE         ,
-  PRIMARY KEY (shohin_id));
+INSERT INTO shohinins (shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka, shiire_tanka, torokubi) VALUES
+('0001', 'Tシャツ', '衣服', 1000, 500, '2009-09-20');
